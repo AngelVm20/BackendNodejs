3dc657e8d2013758729f0c69920ae8ba3239e920
AngelVm20 <84336462+AngelVm20@users.noreply.github.com>
2022-07-08 11:37:26 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const app=express();
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: "Library API",
+        version: '1.0.0',
+      },
+    },
+    apis: ["app.js"],
+  };
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ * 
+ */
+
+app.get('/books', (req, res) => {
+    res.send([
+      {
+        id: 1,
+        title: "Harry Potter",
+      }
+    ])
+});
+
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     description: Get all books
+ *     parameters:
+ *      - name: title
+ *        description: title of the book
+ *        in: formData
+ *        required: true
+ *        type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+
+app.post('/books', (req, res) => {
+    res.status(201).send();
+});
+
+/*pp.delete('/api-docs/books/id'), req.app.get('/books/:bookId', (req, res) => {
+  res.delete(req.params.bookId)
+});*/
+
+
+app.listen(5000, () => console.log("listening on 5000"));
